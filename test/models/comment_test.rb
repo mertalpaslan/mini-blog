946870b0583163a636e44ae38f141c853ce8addb
Mert Alpaslan <alpaslanmert@yahoo.com>
2021-06-05 22:39:57 +0300

    Add model tests for post and comments

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @post = posts(:post_one)
+    @user = users(:user_one)
+  end
+
+  test 'valid comment' do
+    comment = Comment.new(body: 'MyString', user: @user, post: @post)
+    assert comment.valid?
+  end
+
+  test 'invalid comment without body' do
+    comment = Comment.new(user_id: @user.id, post: @post)
+    assert_not_nil comment.errors[:body]
+    refute comment.valid?
+  end
+
+  test 'invalid comment without user' do
+    comment = Comment.new(body: 'MyString', post: @post)
+    assert_not_nil comment.errors[:user]
+    refute comment.valid?
+  end
+
+  test 'invalid comment without post' do
+    comment = Comment.new(body: 'MyString', user: @user)
+    refute comment.valid?
+    assert_not_nil comment.errors[:post]
+  end
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:user_one)
+  end
+
+  test 'valid post' do
+    post = Post.new(title: 'MyString', body: 'MyString', user_id: @user.id)
+    assert post.valid?
+  end
+
+  test 'invalid post without title' do
+    post = Post.new(body: 'MyString', user_id: @user.id)
+    assert_not_nil post.errors[:title]
+    refute post.valid?
+  end
+
+  test 'invalid post without body' do
+    post = Post.new(title: 'MyString', user_id: @user.id)
+    assert_not_nil post.errors[:body]
+    refute post.valid?
+  end
+
+  test 'invalid post without user' do
+    post = Post.new(title: 'MyString', body: 'MyString')
+    refute post.valid?
+    assert_not_nil post.errors[:user]
+  end
 end
